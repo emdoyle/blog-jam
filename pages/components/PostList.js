b39fdc4d356b3a6e77c6de555c50987f2b672025
Evan Doyle <evanmdoyle@gmail.com>
2020-05-24 17:52:43 -0700

    Add a second dummy post and put br after each link

--- pages/components/PostList.js
+++ pages/components/PostList.js
@@ -5,9 +5,12 @@ export default function PostList(props) {
     return (
         <div>
             {props.postNames.map(postName => (
-                <Link key={`${postName}-link`} href={`posts/${postName}`}>
-                    <a>{postName}</a>
-                </Link>
+                <React.Fragment>
+                    <Link key={`${postName}-link`} href={`posts/${postName}`}>
+                        <a>{postName}</a>
+                    </Link>
+                    <br />
+                </React.Fragment>
             ))}
         </div>
     )
