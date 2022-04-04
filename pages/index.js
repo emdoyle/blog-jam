5123544c4fc02640e85017ef92402300d73f1454
Evan Doyle <evanmdoyle@gmail.com>
2022-04-04 15:11:04 -0700

    more logging for vercel

--- pages/index.js
+++ pages/index.js
@@ -5,6 +5,7 @@ import moment from "moment";
 import glob from "glob";
 
 export default function Home(props) {
+  console.log(props);
   return (
     <Layout titlePage lastUpdated={props.lastUpdated}>
       <PostList postNames={props.postNames} />
@@ -17,6 +18,7 @@ export async function getStaticProps() {
   //   parse release date from title
   const posts = glob.sync("**/*.md", { cwd: "posts/" });
   console.log(posts);
+  console.log(posts.map((postFileName) => postFileName.slice(11, -3)));
   return {
     props: {
       postNames: posts.map((postFileName) => postFileName.slice(11, -3)),
